e0c570159d12f839b0b6382ed1f9e948596a1393
ikcore <52038882+ikcore@users.noreply.github.com>
2021-08-05 06:44:56 +0100

    added all relevant tests and cases

--- CheckoutKata.Test/CheckoutTests.cs
+++ CheckoutKata.Test/CheckoutTests.cs
@@ -35,5 +35,122 @@ namespace CheckoutKata.Test
 
             Assert.Null(model);
         }
+
+        [Fact]
+        public void GetSingleItem()
+        {
+            var checkout = GetCheckoutService();
+            checkout.Scan("D");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)15);
+        }
+
+        [Fact]
+        public void GetSingleItemWithSpecial()
+        {
+            var checkout = GetCheckoutService();
+            checkout.Scan("A");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)50);
+        }
+
+        [Fact]
+        public void GetSingleItemWithMultipleQuantity()
+        {
+            var checkout = GetCheckoutService();
+            
+            checkout.Scan("C");
+            checkout.Scan("C");
+            checkout.Scan("C");
+            checkout.Scan("C");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)4 * 20);
+        }
+
+        [Fact]
+        public void GetSingleItemTriggerSpecial()
+        {
+            var checkout = GetCheckoutService();
+
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)130);
+        }
+
+        [Fact]
+        public void GetSingleItemTriggerSpecialWithExtraQuantity()
+        {
+            var checkout = GetCheckoutService();
+
+            // special
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            // plus 2 extra
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)130 + 50 + 50);
+        }
+
+        [Fact]
+        public void GetMultipleItems()
+        {
+            var checkout = GetCheckoutService();
+            checkout.Scan("A");
+            checkout.Scan("B");
+            checkout.Scan("C");
+            checkout.Scan("D");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)50 + 30 + 20 + 15);
+        }
+
+        [Fact]
+        public void GetMultipleItemsWithTrigger()
+        {
+            var checkout = GetCheckoutService();
+
+            checkout.Scan("A");
+            checkout.Scan("A");
+            checkout.Scan("A");
+
+            checkout.Scan("B");
+            checkout.Scan("B");
+
+            checkout.Scan("C");
+            checkout.Scan("D");
+
+            Assert.Equal(checkout.GetTotalPrice(), (int)130 + 45 + 20 + 15);
+        }
+
+        [Fact]
+        public void GetAllItemsWithEdgeAllCases()
+        {
+            var checkout = GetCheckoutService();
+
+            for(var i=0; i< 8; i++)
+                checkout.Scan("A");
+
+            for (var i = 0; i < 5; i++)
+                checkout.Scan("B");
+
+            for (var i = 0; i < 3; i++)
+                checkout.Scan("C");
+
+            for (var i = 0; i < 9; i++)
+                checkout.Scan("D");
+
+            var total = checkout.GetTotalPrice();
+            int actual =
+                (130 * 2) + (2 * 50) // for A 2 specials + 2 extras
+                + (45 * 2) + 30 // for B 2 specials + 1 extra
+                + (3 * 20) // 3 C's
+                + (9 * 15); // 9 D's
+
+            Assert.Equal(total, actual);
+        }
     }
 }
