362c0173299b02e02c7de123938e647580e03d51
ikcore <52038882+ikcore@users.noreply.github.com>
2021-08-05 05:54:53 +0100

    Add models and repository interface

---
+++ CheckoutKata.Domain/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Domain
+{
+    public interface IItemRepository
+    {
+        ItemModel GetItemBySKU(string sku);
+    }
+}
---
+++ CheckoutKata.Domain/Models/ItemModel.cs
@@ -0,0 +1,11 @@
+﻿using CheckoutKata.Domain.Models;
+
+namespace CheckoutKata.Domain
+{
+    public class ItemModel
+    {
+        public string SKU { get; set; }
+        public int Price { get; set; }
+        public ItemSpecialPriceModel SpecialPrice { get; set; }
+    }
+}
